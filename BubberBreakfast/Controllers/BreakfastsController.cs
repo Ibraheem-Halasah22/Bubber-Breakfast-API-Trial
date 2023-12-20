92663abbb9c30fa7e9a5decf103b1f9f3e40dbc7
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-12-20 21:48:10 +0300

    create breakfast request added and tested correctly

--- BubberBreakfast/Controllers/BreakfastsController.cs
+++ BubberBreakfast/Controllers/BreakfastsController.cs
@@ -4,30 +4,31 @@ using Microsoft.AspNetCore.Mvc;
 namespace BubberBreakfast.Controllers;
 
 [ApiController]
+[Route("breakfasts")]
 public class BreakfastsController : ControllerBase
 {
-    [HttpPost("/breakfast")]
+    [HttpPost]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request)
     {
         //TODO: Store the breakfast in the repository
         return Ok(request);
     }
 
-    [HttpGet("/breakfast/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id)
     {
         //TODO: retrieve the breakfast from the database
         return Ok(id);
     }
 
-    [HttpPut("/breakfast/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
     {
         //TODO: Perform the upsert on the database
         return Ok(request);
     }
 
-    [HttpDelete("/breakfast/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id)
     {
         //TODO: Perform the actual deleting in the database
